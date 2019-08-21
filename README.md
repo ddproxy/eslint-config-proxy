f4a8545b77feefce569fa5cf1126ffc60e0a09b7
Jon West <jon.west@entercom.com>
2019-08-21 13:09:04 -0600

    Initial commit of configuration pulled from api

---
+++ README.md
@@ -0,0 +1,5 @@
+# Entercom JavaScript Style Guide
+
+*A biased but reasonable approach to JavaScript*
+
+> **Note**: this configuration extends airbnb configuration which can be installed with `npm i -s eslint-config-airbnb`
---
+++ index.js
@@ -0,0 +1,75 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base'
+  ],
+  overrides: [
+    {
+      files: [
+        'test/**/*_spec.js'
+      ],
+      plugins: [
+        'no-only-tests',
+        'mocha'
+      ],
+      env: {
+        node: true,
+        mocha: true,
+        es6: true
+      },
+      rules: {
+        'no-only-tests/no-only-tests': 2
+      }
+    }
+  ],
+  rules: {
+    'jsdoc/no-undefined-types': 0,
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreTemplateLiterals: true,
+        ignorePattern: '(^\\s*it\\(\\\'.+\\\',$)|(^const\\s\\w+\\s=\\srequire\\()'
+      }
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxBOF: 0,
+        maxEOF: 1
+      }
+    ],
+    camelcase: 0,
+    'object-curly-newline': [
+      'error',
+      {
+        consistent: true
+      }
+    ],
+    'no-confusing-arrow': 0,
+    'no-plusplus': 0,
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement'
+    ]
+  }
+};
